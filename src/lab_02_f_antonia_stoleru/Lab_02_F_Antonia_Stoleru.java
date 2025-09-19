3433294f428fd5b05c285489b83b21c3c25faa68
Antonia Stoleru <2484936@edu.vaniercollege.qc.ca>
2025-09-19 13:35:01 -0400

    Added base structure of grid, labels and textfields

--- src/lab_02_f_antonia_stoleru/Lab_02_F_Antonia_Stoleru.java
+++ src/lab_02_f_antonia_stoleru/Lab_02_F_Antonia_Stoleru.java
@@ -4,17 +4,74 @@
  */
 package lab_02_f_antonia_stoleru;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
 /**
  *
  * @author 2484936
  */
-public class Lab_02_F_Antonia_Stoleru {
+public class Lab_02_F_Antonia_Stoleru extends Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch();
     }
-    
+
+    @Override
+    public void start(Stage stage) {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        
+        
+        Label days = new Label("Number of days on the trip:");
+        grid.add(days, 0, 0);
+        TextField d = new TextField();
+        grid.add(d, 1, 0);
+        
+        Label airfare = new Label("Amount of airfare:");
+        grid.add(airfare, 0, 1);
+        TextField a = new TextField();
+        grid.add(a, 1, 1);
+        
+        Label carRental = new Label("Amount of car rental fees:");
+        grid.add(carRental, 0, 2);
+        TextField cr = new TextField();
+        grid.add(cr, 1, 2);;
+        
+        Label miles = new Label("Number of miles driven");
+        grid.add(miles, 0, 3);
+        TextField m = new TextField();
+        grid.add(m, 1, 3);
+        
+        Label parking = new Label("Amount of parking fees");
+        grid.add(parking, 0, 4);
+        TextField p = new TextField();
+        grid.add(p, 1, 4);
+        
+        Label taxi = new Label("Amount of taxi charges");
+        grid.add(taxi, 0, 5);
+        TextField t = new TextField();
+        grid.add(t, 1, 5);
+        
+        Label lodging = new Label("Lodging charges, per night");
+        grid.add(lodging, 0, 6);
+        TextField l = new TextField();
+        grid.add(l, 1, 6);
+        
+        Scene scene = new Scene(grid, 300, 375);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
