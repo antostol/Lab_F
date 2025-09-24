bbfec04be3a0203bc0ec22792c5c1046aaecedc3
antostol <antoniastoleru24@gmail.com>
2025-09-24 11:12:58 -0400

    Added title and modified certain necessary things

--- src/lab_02_f_antonia_stoleru/Lab_02_F_Antonia_Stoleru.java
+++ src/lab_02_f_antonia_stoleru/Lab_02_F_Antonia_Stoleru.java
@@ -1,10 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+
+// link to git repository if necessary: https://github.com/antostol/Lab_F
  */
 package lab_02_f_antonia_stoleru;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -35,57 +38,66 @@ public class Lab_02_F_Antonia_Stoleru extends Application {
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
         
+        Label title = new Label("Travel Expenses Calculator");
+        grid.add(title, 0, 0, 2, 1);
+        GridPane.setHalignment(title, javafx.geometry.HPos.CENTER);
         
         Label days = new Label("Number of days on the trip:");
-        grid.add(days, 0, 0);
+        grid.add(days, 0, 1);
         TextField d = new TextField();
-        grid.add(d, 1, 0);
+        grid.add(d, 1, 1);
         
         Label airfare = new Label("Amount of airfare:");
-        grid.add(airfare, 0, 1);
+        grid.add(airfare, 0, 2);
         TextField a = new TextField();
-        grid.add(a, 1, 1);
+        grid.add(a, 1, 2);
         
         Label carRental = new Label("Amount of car rental fees:");
-        grid.add(carRental, 0, 2);
+        grid.add(carRental, 0, 3);
         TextField cr = new TextField();
-        grid.add(cr, 1, 2);
+        grid.add(cr, 1, 3);
         
         Label miles = new Label("Number of miles driven with private vehicle:");
-        grid.add(miles, 0, 3);
+        grid.add(miles, 0, 4);
         TextField m = new TextField();
-        grid.add(m, 1, 3);
+        grid.add(m, 1, 4);
         
         Label parking = new Label("Amount of parking fees:");
-        grid.add(parking, 0, 4);
+        grid.add(parking, 0, 5);
         TextField p = new TextField();
-        grid.add(p, 1, 4);
+        grid.add(p, 1, 5);
         
         Label taxi = new Label("Amount of taxi charges:");
-        grid.add(taxi, 0, 5);
+        grid.add(taxi, 0, 6);
         TextField t = new TextField();
-        grid.add(t, 1, 5);
+        grid.add(t, 1, 6);
         
         Label conference = new Label("Conference/Seminar fees:");
-        grid.add(conference, 0, 6);
+        grid.add(conference, 0, 7);
         TextField c = new TextField();
-        grid.add(c, 1, 6);
+        grid.add(c, 1, 7);
         
         
         Label lodging = new Label("Lodging charges, per night:");
-        grid.add(lodging, 0, 7);
+        grid.add(lodging, 0, 8);
         TextField l = new TextField();
-        grid.add(l, 1, 7);
+        grid.add(l, 1, 8);
         
         Label result = new Label();
         result.setVisible(false);
-        grid.add(result, 0, 9, 2, 1);
+        grid.add(result, 0, 10, 2, 1);
         
         Button calculate = new Button("Calculate total travel expenses");
-        grid.add(calculate, 0, 8, 2, 1);
+        grid.add(calculate, 0, 9, 2, 1);
         
-        Scene scene = new Scene(grid, 300, 375);
+        Scene scene = new Scene(grid, 300, 450);
         stage.setScene(scene);
+        
+        stage.setMaximized(true);
+        
+        grid.prefWidthProperty().bind(scene.widthProperty());
+        grid.prefHeightProperty().bind(scene.heightProperty());
+        
         stage.show();
         
         calculate.setOnAction(e -> {
